fe3fdf913fd6b85b8e036914d552c64aac51dfae
Leandro Paixão <leandro.tec.inf@gmail.com>
2022-03-18 12:40:32 -0400

    Ajouté le méthode GetTexteSepareDuCamelCase

--- Exemple1.cs
+++ Exemple1.cs
@@ -4,6 +4,11 @@ namespace Strings.Outils
 {
     public static class OutilsPourStrings
     {
-        
+        public static string GetTexteSepareDuCamelCase(string texte)
+        {
+            // Recoit une texte dans le format GetTextCamelCase
+            // et retourne le texte separé Get Text Camel Case
+            return Regex.Matches(requestName, @"[A-Z]{1}[a-z]{1,}").Trim();
+        }
     }
 }
